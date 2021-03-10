e0fba7b4ac633860d55aa053af723856b9df4f83
Thang Le Quang <lethang7794@gmail.com>
2021-03-10 16:38:17 +0700

    Add user, product, order model

---
+++ server/models/order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'Order' },
+    products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    status: { type: String, emum: ['pending', 'paid'], default: 'pending' },
+    total: { type: Number, default: 0 },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamp: true }
+);
+
+orderSchema.plugin(require('./plugins/isDeletedFalse'));
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
---
+++ server/models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [{ imageUrl: { type: String, required: true } }],
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamp: true }
+);
+
+productSchema.plugin(require('./plugins/isDeletedFalse'));
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
