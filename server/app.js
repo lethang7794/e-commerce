39d299264ae8e890001691ca60c22a3a8adc02af
Thang Le Quang <lethang7794@gmail.com>
2021-03-10 14:44:01 +0700

    Install, set up dotenv

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('dotenv').config();
+
 var app = express();
 
 app.use(logger('dev'));
