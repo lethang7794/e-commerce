754c724f7515be25dbffb03f1c86c5556963132f
Thang Le Quang <lethang7794@gmail.com>
2021-03-10 15:37:50 +0700

    Mount backend route to /api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const usersRouter = require('./users');
+
+router.use('/users', usersRouter);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send('E-commerce backend');
 });
 
 module.exports = router;
