0bcabec3a9cd6c8ce72cf59777d251bb258d5a71
Thang Le Quang <lethang7794@gmail.com>
2021-03-10 15:31:20 +0700

    Add utils helper to send response

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const utilsHelper = require('./helpers/utils.helper');
+
 require('dotenv').config();
 
 var app = express();
