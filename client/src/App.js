28f89567f6f42d181cc110c5139f7b4e22a578d9
Thang Le Quang <lethang7794@gmail.com>
2021-03-12 13:58:00 +0700

    Add navigation bar, update index head

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import PublicNavBar from './components/PublicNavBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <PublicNavBar />
+      <Container>
+        <h1 className='text-center'>Let's go</h1>
+      </Container>
     </div>
   );
 }
